6b85a6adcb9028ba59309b145e3f18ebd5b2b910
Paulo <kpaulopp@gmail.com>
2025-08-18 11:10:56 -0300

    mudando a sitaxes

--- ex004.py
+++ ex004.py
@@ -1,4 +1,4 @@
 n1 =  int(input('primeiro numero = '))
 n2 = int(input ('segundo numero = '))
 soma = n1 + n2
-print('A soma dos', n1, 'mais o ', n2,' e igual a {}'.format(soma))
+print('A soma dos {} e {} e igual a {}'.format(n1, n2, soma))
