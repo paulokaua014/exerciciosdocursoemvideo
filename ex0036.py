5a3d00ec186e45ee40f151f86a247578f7abe4a5
Paulo <kpaulopp@gmail.com>
2025-09-15 08:11:13 -0300

    corecao dos exercicios

--- ex0036.py
+++ ex0036.py
@@ -2,7 +2,7 @@ valorcasa = float(input('Qual o valor da casa? R$ '))
 salario = float(input('Qual o seu salário? R$ '))
 anos = int(input('Em quantos anos você pretende pagar? '))
 prestacao = valorcasa / (anos * 12)
-minimo = salario * 0.3
+minimo = salario * 30 / 100
 print('Para pagar uma casa de R$ {:.2f} em {} anos'.format(valorcasa, anos), end='')
 print(' a prestação será de R$ {:.2f}'.format(prestacao))
 if prestacao <= minimo:
