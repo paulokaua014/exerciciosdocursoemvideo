0087fb7a05e86cd24c381f5060694efcb79aee11
Paulo <kpaulopp@gmail.com>
2025-09-15 08:19:50 -0300

    correcao e mundaca do codigo do ex39

--- ex0039.py
+++ ex0039.py
@@ -1,9 +1,17 @@
-ano = int (input('Em que ano você nasceu? '))
-idade = 2024 - ano
-if idade < 18:
-    print('Você ainda vai se alistar ao serviço militar.')
-    print('Ainda faltam {} anos para o alistamento.'.format(18 - idade))
-elif idade == 18:
+from datetime import date
+atual = date.today().year
+nasc = int(input('Em que ano você nasceu? '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
     print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade < 18:
+    saldo = 18 - idade
+    ano = atual + saldo
+    print('Ainda faltam {} anos para o alistamento.'.format(saldo))
+    print('Seu alistamento será em {}.'.format(ano))
 else:
-    print('Você já deveria ter se alistado há {} anos.'.format(idade - 18))
+    saldo = idade - 18
+    ano = atual - saldo
+    print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+    print('Seu alistamento foi em {}.'.format(ano))
