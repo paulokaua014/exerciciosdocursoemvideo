a92fb474d0461720cab9ba02994cc887e95c2058
Paulo <kpaulopp@gmail.com>
2025-09-15 09:00:42 -0300

    correcao do codigo do exercicio 44

---
+++ ex0044.py
@@ -0,0 +1,26 @@
+print('{:=^40}'.format('LOJA DO PAULO '))
+preco = float(input('Preço das compras: R$ '))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opcao = int(input('Escolha a forma de pagamento: '))
+if opcao == 1:
+    total = preco - (preco * 10 / 100)
+elif opcao == 2:
+    total = preco - (preco * 5 / 100)
+elif opcao == 3:
+    total = preco
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R$ {:.2f} SEM JUROS'.format(parcela))
+elif opcao == 4:
+    total = preco + (preco * 20 / 100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R$ {:.2f} COM JUROS'.format(totparc, parcela))
+else:
+    total = preco
+    print('Opção inválida de pagamento. Tente novamente!')
+
+print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(preco, total))
