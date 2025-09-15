45a1d7b52d180dc306e28213d13e3699c35afb48
Paulo <kpaulopp@gmail.com>
2025-09-15 08:42:06 -0300

    correcao do exercicio 42

--- exx042.py
+++ exx042.py
@@ -4,12 +4,14 @@ reta2 = float(input('Digite o comprimento da segunda reta: '))
 reta3 = float(input('Digite o comprimento da terceira reta: '))
 print('Analisando as retas...')
 sleep(3)
-if reta1 < reta2 + reta3:
-    print('Com essas retas é possível formar um triângulo!')
-elif reta2 == reta1 == reta3:
-    print('Com essas retas é possível formar um triângulo equilátero!')
-elif reta1 == reta2 or reta1 == reta3 or reta2 == reta3:
-    print('Com essas retas é possível formar um triângulo isósceles!')
-
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    print('Com essas retas é possível formar um triângulo!', end=' ')
+    if reta1 == reta2 == reta3:
+        print('EQUILÁTERO!')
+    elif reta1 != reta2 != reta3 != reta1:
+        print('ESCALENO!')
+    else:
+        print('ISÓSCELES!')
+    
 else:
     print('Com essas retas não é possível formar um triângulo!')
