9378de1685520e9bd85aab618c481479ed2cfb99
Paulo <kpaulopp@gmail.com>
2025-09-15 08:23:26 -0300

    correcao do exercicio 40

--- ex0040.py
+++ ex0040.py
@@ -1,9 +1,9 @@
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
-if media < 5.0:
-    print('Sua média foi {:.1f}. Você está REPROVADO.'.format(media))
+if 7 > media < 5.0:
+    print('Sua média foi {:.1f}, Você está REPROVADO.'.format(media))
 elif 5.0 <= media < 7.0:
-    print('Sua média foi {:.1f}. Você está de RECUPERAÇÃO.'.format(media))
+    print('Sua média foi {:.1f}, Você está de RECUPERAÇÃO.'.format(media))
 else: 
-    print('Sua média foi {:.1f}. PARABÉNS! Você está APROVADO.'.format(media))
+    print('Sua média foi {:.1f}, PARABÉNS! Você está APROVADO.'.format(media))
